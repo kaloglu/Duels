dd58275da43002859482cecd96512bcf52bf037d
kaloglu <cihan@kaloglu.com>
2019-01-27 23:26:43 +0300

    Simplfy UI state manager

--- app/src/main/java/com/kaloglu/duels/mobileui/interfaces/UIStateManager.kt
+++ app/src/main/java/com/kaloglu/duels/mobileui/interfaces/UIStateManager.kt
@@ -1,69 +1,56 @@
 package com.kaloglu.duels.mobileui.interfaces
 
 import android.app.Activity
-import android.content.Context
 import android.view.View
 import androidx.annotation.UiThread
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.transition.TransitionManager
 import com.kaloglu.duels.injection.scopes.PerFragment
+import com.kaloglu.duels.utils.hide
+import com.kaloglu.duels.utils.show
 import javax.inject.Inject
 
 @PerFragment
 class UIStateManager @Inject constructor(val context: Activity) {
 
+    private lateinit var statesView: UIStatesView
+
+    enum class UIStateType {
+        LOADING,
+        EMPTY,
+        CONTENT,
+        ERROR
+    }
+
     interface UIStatesView {
-        fun getLoadingContainer(): View?
-        fun getEmptyContainer(): View?
-        fun getContentContainer(): View?
-        fun getErrorContainer(): View?
 
         fun getSceneLayout(): ConstraintLayout?
+        fun getContainer(uiStateType: UIStateType): View?
     }
 
     interface UIStatesPresenter {
-        @UiThread
-        fun loadingUIState(): Unit?
 
         @UiThread
-        fun emptyUIState(): Unit?
-
-        @UiThread
-        fun contentUIState(): Unit?
-
-        @UiThread
-        fun errorUIState(): Unit?
+        fun getUIState(state: UIStateType): Unit?
     }
-
-    private lateinit var statesView: UIStatesView
 
     fun initStates(statesView: UIStatesView) {
         this.statesView = statesView
     }
 
-    private fun showUIState(view: View) {
-        statesView.getSceneLayout()?.run {
-            beginDelayed(this)
-            hideAllState()
-            view.visibility = View.VISIBLE
+    fun getState(state: UIStateType) = statesView.showUIState(state)
+
+    private fun UIStatesView.showUIState(state: UIStateType) {
+        getSceneLayout()?.let(TransitionManager::beginDelayedTransition)
+        hideAllState(this)
+        getContainer(state).show()
+    }
+
+    private fun hideAllState(uiStatesView: UIStatesView) {
+        UIStateType.values().forEach {
+            uiStatesView.getContainer(it).hide()
         }
     }
 
-    private fun hideAllState() {
-        statesView.getLoadingContainer()?.visibility = View.GONE
-        statesView.getEmptyContainer()?.visibility = View.GONE
-        statesView.getContentContainer()?.visibility = View.GONE
-        statesView.getErrorContainer()?.visibility = View.GONE
-    }
-
-    fun loadingUIState() = statesView.getLoadingContainer()?.run(::showUIState)
-
-    fun emptyUIState() = statesView.getEmptyContainer()?.run(::showUIState)
-
-    fun contentUIState() = statesView.getContentContainer()?.run(::showUIState)
-
-    fun errorUIState() = statesView.getErrorContainer()?.run(::showUIState)
-
-    private fun beginDelayed(constraintLayout: ConstraintLayout) = TransitionManager.beginDelayedTransition(constraintLayout)
-
 }
+
