9e4f7a4efccc8912f032ca450dedeba1965351fa
kaloglu <cihan@kaloglu.com>
2019-01-27 23:28:05 +0300

    Add state management for tournament

---
+++ app/src/main/java/com/kaloglu/duels/data/model/Tournament.kt
@@ -0,0 +1,19 @@
+package com.kaloglu.duels.data.model
+
+import com.google.firebase.firestore.IgnoreExtraProperties
+
+import com.kaloglu.duels.utils.empty
+
+/**
+ * Tournament POJO.
+ */
+@IgnoreExtraProperties
+data class Tournament constructor(
+        val name: String? = String.empty
+) : BaseModel() {
+
+    companion object {
+
+        val FIELD_NAME = "name"
+    }
+}
---
+++ app/src/main/java/com/kaloglu/duels/data/repository/tournaments/TournamentsRepository.kt
@@ -0,0 +1,56 @@
+package com.kaloglu.duels.data.repository.tournaments
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.kaloglu.duels.data.model.Tournament
+import com.kaloglu.duels.domain.tournaments.FireStoreLiveData
+import com.kaloglu.duels.injection.scopes.PerApplication
+import com.kaloglu.duels.data.filters.Filters
+import timber.log.Timber
+import javax.inject.Inject
+
+@PerApplication
+class TournamentsRepository @Inject constructor(private val firestore: FirebaseFirestore) {
+
+    fun getTournaments(filters: Filters?): FireStoreLiveData<Tournament> =
+            FireStoreLiveData(toQuery(filters), Tournament::class.java)
+
+    private fun toQuery(filters: Filters?): Query {
+        // Construct query basic query
+        var query: Query = firestore.collection("tournaments")
+        query.orderBy(Tournament.FIELD_NAME, Query.Direction.ASCENDING)
+
+        filters?.run {
+            // Name (equality filter)
+            name?.let {
+                query = query.whereEqualTo(Tournament.FIELD_NAME, it)
+            }
+
+        }
+
+        /* query could be limited like: query.limit(5) */
+        return query
+    }
+
+    fun addTournament(tournament: Tournament/*, collabrators:List<Collabrator>*/) {
+        val batch = firestore.batch()
+        val restRef = firestore.collection("tournaments").document()
+
+        // Add restaurant
+        batch.set(restRef, tournament)
+//        batch.set(restRef.collection("collabrators").document(), collabrators)
+
+        batch.commit().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Timber.d("Write batch succeeded.")
+            } else {
+                Timber.w("write batch failed.", task.exception)
+            }
+        }
+    }
+
+    fun removeTournament(id: String) {
+        firestore.collection("tournaments").document(id).delete()
+    }
+}
+
