72259272341c6e72e336e28c02db542bc719b82c
kaloglu <cihan@kaloglu.com>
2019-02-24 21:45:45 +0300

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    ./gradlew postBeta --stacktrace --no-daemon
     git config --global user.email "build@travis-ci.com"
     git config --global user.name "Travis CI"
     echo 'add remote'
     git remote add release "https://$GH_TOKEN@github.com/kaloglu/Duels.git"
+    echo 'checkout $TRAVIS_BRANCH'
+    git checkout $TRAVIS_BRANCH
+    ./gradlew postBeta
     echo 'log $TRAVIS_COMMIT'
     git log --format=%B -n 1 $TRAVIS_COMMIT > beta_release_notes.txt
     echo 'add -u'
     git add . -u
     echo 'tag version $GIT_TAG_VERSION'
     git commit -m "[ci skip] Setting version to $GIT_TAG_VERSION"
-    echo 'checkout $TRAVIS_BRANCH'
-    git checkout $TRAVIS_BRANCH
     echo 'tagged version $GIT_TAG_VERSION'
     git tag v$GIT_TAG_VERSION -a -m "Tagging version v$GIT_TAG_VERSION"
     echo 'push release $TRAVIS_BRANCH'
