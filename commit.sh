5d525a7bccceedd1795b170fd85ba0a6ce781a83
kaloglu <cihan@kaloglu.com>
2019-02-24 22:11:56 +0300

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 set -e
-if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
     git config --global user.email "build@travis-ci.com"
     git config --global user.name "Travis CI"
     echo 'add remote'
     git remote add release "https://$GH_TOKEN@github.com/kaloglu/Duels.git"
     echo 'checkout $TRAVIS_BRANCH'
     git checkout $TRAVIS_BRANCH
+
+latest=$(git tag  -l --merged master --sort='-*authordate' | head -n1)
+ 
+semver_parts=(${latest//./ })
+major=${semver_parts[0]}
+minor=${semver_parts[1]}
+patch=${semver_parts[2]}
+ 
+count=$(git rev-list HEAD ^${latest} --ancestry-path ${latest} --count)
+version=""
+ 
+case $branch in
+   "master")
+      version=${major}.$((minor+1)).0
+      ;;
+   "feature/*")
+      version=${major}.${minor}.${patch}-${branch}-${count}
+      ;;
+   *)
+      >&2 echo "unsupported branch type"
+      exit 1
+      ;;
+esac
+ 
+echo ${version}
+
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
     ./gradlew postBeta
     echo 'log $TRAVIS_COMMIT'
     git log --format=%B -n 1 $TRAVIS_COMMIT > beta_release_notes.txt
     echo 'add -u'
     git add . -u
-    echo 'tag version $GIT_TAG_VERSION'
-    git commit -m "[ci skip] Setting version to $GIT_TAG_VERSION"
-    echo 'tagged version $GIT_TAG_VERSION'
-    git tag v$GIT_TAG_VERSION -a -m "Tagging version v$GIT_TAG_VERSION"
+    echo 'tag version ${version}'
+    git commit -m "[ci skip] Setting version to ${version}"
     echo 'push release $TRAVIS_BRANCH'
     git push -f release $TRAVIS_BRANCH 2>&1
-    git push -f release $TRAVIS_BRANCH --tags 2>&1
     ./gradlew crashlyticsUploadDistributionFirebaseBeta --stacktrace --no-daemon
 fi
+    echo 'tagged version ${version}'
+    git tag v${version} -a -m "Tagging version v${version}"
+    git push -f release $TRAVIS_BRANCH --tags 2>&1
