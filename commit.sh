61fe3941313853b42c322cdac239dc2a59668535
Travis CI <build@travis-ci.com>
2019-03-02 15:29:43 +0000

    fix ci

--- commit.sh
+++ commit.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
-set -e
-    ./gradlew postBeta
+if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    set -e
     git config --global user.email "build@travis-ci.com"
     git config --global user.name "Travis CI"
     git remote add release "https://$GH_TOKEN@github.com/kaloglu/Duels.git"
     git checkout $TRAVIS_BRANCH
 
-file="./app/version.properties"
+    file="./app/version.properties"
 
-if [[ -f "$file" ]]; then
-  while IFS='=' read -r key value
-  do
-    key=$(echo ${key} | tr '.' '_')
-    eval ${key}=\$value
-  done < "$file"
+    if [[ -f "$file" ]]; then
+        while IFS='=' read -r key value
+        do
+            key=$(echo ${key} | tr '.' '_')
+            eval ${key}=\$value
+        done < "$file"
 
-  echo "major       = " ${VERSION_MAJOR}
-  echo "minor       = " ${VERSION_MINOR}
-  echo "patch       = " ${VERSION_PATCH}
-else
-  echo "$file not found."
-fi
- 
-version=${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
- 
-echo 'final ver =》 '.${version}
+        echo "major       = " ${VERSION_MAJOR}
+        echo "minor       = " ${VERSION_MINOR}
+        echo "patch       = " ${VERSION_PATCH}
+    else
+        echo "$file not found."
+    fi
 
-if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    version=${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
+
     git log $(git describe --tags --abbrev=0)..HEAD --oneline > beta_release_notes.txt
+    ./gradlew crashlyticsUploadDistributionFirebaseBeta
     git add . -u
     git commit -m "[ci skip] ${version}"
     git push -f release $TRAVIS_BRANCH 2>&1
     git tag beta/${version} -a -m "[ci tagging] beta/${version}"
     git push -f release $TRAVIS_BRANCH --tags 2>&1
-
-    ./gradlew crashlyticsUploadDistributionFirebaseBeta
+    ./gradlew postBeta
 fi
