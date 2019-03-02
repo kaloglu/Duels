7d2f3b80bbd9c54c05f5ce00791f9939bc704f19
kaloglu <cihan@kaloglu.com>
2019-03-02 15:35:43 +0300

    Fix ci processes

--- commit.sh
+++ commit.sh
@@ -2,9 +2,7 @@
 set -e
     git config --global user.email "build@travis-ci.com"
     git config --global user.name "Travis CI"
-    echo 'add remote'
     git remote add release "https://$GH_TOKEN@github.com/kaloglu/Duels.git"
-    echo 'checkout $TRAVIS_BRANCH'
     git checkout $TRAVIS_BRANCH
 
 file="./app/version.properties"
@@ -32,15 +30,12 @@ echo 'final ver =》 '.${version}
 
 if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
     git log $(git describe --tags --abbrev=0)..HEAD --oneline > beta_release_notes.txt
-    ./gradlew clean assembleFirebaseBeta postBeta crashlyticsUploadDistributionFirebaseBeta
-    echo 'log '.$TRAVIS_COMMIT
-    echo 'add -u'
+    ./gradlew crashlyticsUploadDistributionFirebaseBeta
+    ./gradlew postBeta
     git add . -u
-    echo 'tag version '.${version}
-    git commit -m "[ci skip] Setting version to ${version}"
-    echo 'push release '.$TRAVIS_BRANCH
+    git commit -m "[ci skip] ${version}"
     git push -f release $TRAVIS_BRANCH 2>&1
-fi
-    echo 'tagged version '.${version}
-    git tag beta/${version} -a -m "Tagging version beta/${version}"
+    git tag beta/${version} -a -m "[ci tagging] beta/${version}"
     git push -f release $TRAVIS_BRANCH --tags 2>&1
+
+fi
